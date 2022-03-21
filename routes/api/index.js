f32af37103e12b7738697936d8ab8ca6056e3e85
kbugz <kbugusky@gmail.com>
2022-03-21 17:00:58 -0500

    ready to deploy to Heroku

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-router.use('/users', userRoutes); // Allows you to prepend all urls in user routes with /users
-router.use('/posts', postRoutes);// Allows you to prepend all urls in user routes with /posts
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
